cc9d6174af6c6c07010980047391f17f2dc9f852
ericmartin33 <7yx9zUneaHPjn3g>
2023-03-10 11:53:15 +0100

    Implements signin & signup screens

--- reminder_app/lib/screens/auth/signin_screen.dart
+++ reminder_app/lib/screens/auth/signin_screen.dart
@@ -9,6 +9,18 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,15 +35,53 @@ class _SignInScreenState extends State<SignInScreen> {
           )
         ],
       ),
-      body: Column(children: [
-        const SizedBox(
-          height: 200,
-        ),
-        Text(
-          'Une appli quelle est belle',
-          style: Theme.of(context).textTheme.headlineSmall,
-        )
-      ]),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          const SizedBox(
+            height: 200,
+          ),
+          Text(
+            'Une appli quelle est belle',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    TextFormField(
+                        decoration:
+                            const InputDecoration(hintText: 'Enter Email'),
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _emailController,
+                        validator: (value) =>
+                            value == null || !value.contains('@')
+                                ? 'L\'email n\'est pas valide'
+                                : null),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                        controller: _passwordController,
+                        decoration:
+                            const InputDecoration(hintText: 'Enter Password'),
+                        obscureText: true,
+                        validator: (value) => value!.length < 6
+                            ? 'Le mot de passe doit faire au moins 6 caracteres'
+                            : null),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            print('submit form');
+                          }
+                        },
+                        child: const Text('Sign In'))
+                  ],
+                )),
+          ),
+        ]),
+      ),
     );
   }
 }
--- reminder_app/lib/screens/auth/signup_screen.dart
+++ reminder_app/lib/screens/auth/signup_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -11,6 +10,18 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +35,53 @@ class _SignUpScreenState extends State<SignUpScreen> {
             label: const Text('Sign In'),
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          const SizedBox(
+            height: 200,
+          ),
+          Text(
+            'Une appli quelle est belle',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    TextFormField(
+                        decoration:
+                            const InputDecoration(hintText: 'Enter Email'),
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _emailController,
+                        validator: (value) =>
+                            value == null || !value.contains('@')
+                                ? 'L\'email n\'est pas valide'
+                                : null),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                        controller: _passwordController,
+                        decoration:
+                            const InputDecoration(hintText: 'Enter Password'),
+                        obscureText: true,
+                        validator: (value) => value!.length < 6
+                            ? 'Le mot de passe doit faire au moins 6 caracteres'
+                            : null),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            print('submit form');
+                          }
+                        },
+                        child: const Text('Sign Up'))
+                  ],
+                )),
+          ),
+        ]),
       ),
     );
   }
